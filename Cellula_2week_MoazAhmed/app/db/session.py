29287a73661cedb381b1b3f0a78611557e783cf7
Moaz715 <moazfathy592005@gmail.com>
2026-07-18 19:17:11 +0300

    quantization report

--- Cellula_2week_MoazAhmed/app/db/session.py
+++ Cellula_2week_MoazAhmed/app/db/session.py
@@ -6,41 +6,7 @@ import csv
 import os
 from datetime import datetime, UTC
 
-# This function searches your project for the .env file and loads the variables
 load_dotenv()
-
-class SQLiteDatabaseManager:
-    def __init__(self):
-        # os.getenv pulls the variable. The second argument is a fallback just in case.
-        self.db_path = os.getenv("DB_PATH", "api_logs.db")
-        self._initialize_db()
-        
-    def _initialize_db(self):
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute("PRAGMA journal_mode=WAL;")
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS logs (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    timestamp TEXT,
-                    input_type TEXT,
-                    content TEXT,
-                    prediction TEXT
-                )
-            """)
-            conn.commit()
-            
-    def log_entry(self, input_type: str, content: str, prediction: str):
-        with sqlite3.connect(self.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute(
-                "INSERT INTO logs (timestamp, input_type, content, prediction) VALUES (?, ?, ?, ?)",
-                (datetime.now().isoformat(), input_type, content, prediction)
-            )
-            conn.commit()
-            
-            
-
 
 class CSVDatabaseManager:
     def __init__(self):
@@ -50,7 +16,6 @@ class CSVDatabaseManager:
         self._initialize_file()
 
     def _initialize_file(self):
-        """Creates the CSV file and writes headers if it doesn't exist."""
         file_exists = os.path.isfile(self.filepath)
         
         if not file_exists:
@@ -59,7 +24,6 @@ class CSVDatabaseManager:
                 writer.writerow(self.headers)
 
     def log_entry(self, input_type: str, content: str, prediction: str):
-        """Appends a single prediction event to the CSV."""
         timestamp = datetime.now(UTC).isoformat()
         
         clean_content = content.replace("\n", " ").replace("\r", " ")
